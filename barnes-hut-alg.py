21541f2998ae025207110c0dfa63ae108e5daee8
Jigsy <astrojigs24@gmail.com>
2022-01-27 12:08:16 +0000

    nice update

--- barnes-hut-alg.py
+++ barnes-hut-alg.py
@@ -2,10 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle as rect
 import utils
+fig, ax = plt.subplots(figsize=(5,5))
 
 boundary = utils.Rectangle(200,200,200,200)
 qt = utils.Quadtree(boundary, 4)
 
-for i in range(50):
+for i in range(10):
     p = utils.Point(np.random.uniform(0,400),np.random.uniform(0,400))
+    ax.scatter(p.x,p.y,s=1,c='black')
     qt.insert(p)
+
+ax.set_xlim(0,400)
+ax.set_ylim(0,400)
+
+qt.show(ax)
+plt.show()
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as patches
 class Point:
     def __init__(self,x,y):
         self.x = x
@@ -14,22 +14,14 @@ class Rectangle:
         self.h = h
 
     def contains(self,point):
-        if (point.x > self.x-self.w and point.x < self.x + self.w and
-        point.y > self.y - self.h and point.y < self.y + self.h):
-            return True
-        else:
-            return False
+        return (point.x >= self.x-self.w and point.x <= self.x + self.w and
+        point.y >= self.y - self.h and point.y <= self.y + self.h)
 
 
 class Quadtree:
     def __init__(self,boundary,n):
 
         self.boundary = boundary
-
-        self.x = self.boundary.x
-        self.y = self.boundary.y
-        self.w = self.boundary.w
-        self.h = self.boundary.h
 
         # choosing capacity(n) = 4, i.e. if particle number crosses 4 than sub-divide
         # When do i choose that i need to sub-divide
@@ -47,36 +39,49 @@ class Quadtree:
         w = self.boundary.w
         h = self.boundary.h
 
-        ne = Rectangle(x + w/2,y - h/2,w/2,h/2)
-        nw = Rectangle(x - w/2,y - h/2,w/2,h/2)
-        se = Rectangle(x + w/2,y + h/2,w/2,h/2)
-        sw = Rectangle(x - w/2,y + h/2,w/2,h/2)
-
-        self.northwest = Quadtree(nw,self.capacity);
+        ne = Rectangle(x + w/4,y - h/4, w/2,h/2)
         self.northeast = Quadtree(ne,self.capacity);
+        nw = Rectangle(x - w/4,y - h/4, w/2,h/2)
+        self.northwest = Quadtree(nw,self.capacity);
+        se = Rectangle(x + w/4,y + h/4, w/2,h/2)
         self.southeast = Quadtree(se,self.capacity);
+        sw = Rectangle(x - w/4,y + h/4, w/2,h/2)
         self.southwest = Quadtree(sw,self.capacity);
 
+        self.divided=True
+
     def insert(self,point):
-        if self.boundary.contains(point) == False:
+        if self.boundary.contains(point) != True:
+            print("point not in boundary")
             return
 
-
+        print("point is in boundary!")
         if (len(self.points) < self.capacity):
+            print('point is add')
             self.points.append(point)
-        else:
-            if (self.divided == False):
-                self.subdivide()
-                self.divided=True
-                self.northeast.insert(point)
-                self.northwest.insert(point)
-                self.southeast.insert(point)
-                self.southwest.insert(point)
 
-    def show(self):
-        Rectangle(self.boundary.x, self.boundary.y,self.boundary.w/2, self.boundary.h/2)
-        if (self.divided):
-            self.northeast.show()
-            self.northwest.show()
-            self.southeast.show()
-            self.southwest.show()
+        # If there is no division yet, then divide
+        if self.divided != True:
+            self.subdivide()
+            self.divided=True
+
+            self.northeast.insert(point)
+            self.northwest.insert(point)
+            self.southeast.insert(point)
+            self.southwest.insert(point)
+
+    def show(self,axis):
+        axis.add_patch(patches.Rectangle((self.boundary.x, self.boundary.y),
+        self.boundary.w*2, self.boundary.h*2,
+         fill=False))
+
+        if self.divided:
+            self.northeast.show(axis)
+            self.northwest.show(axis)
+            self.southeast.show(axis)
+            self.southwest.show(axis)
+        count=0
+        for p in self.points:
+            count+=1
+            print(count)
+            axis.scatter(p.x,p.y,c='black')
