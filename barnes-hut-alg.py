0e38194c94faba2634d9ce50dea94d80e429c669
Jigsy <astrojigs24@gmail.com>
2022-02-02 00:08:36 +0000

    working update

--- barnes-hut-alg.py
+++ barnes-hut-alg.py
@@ -4,18 +4,23 @@ from matplotlib.patches import Rectangle as rect
 import utils
 fig, ax = plt.subplots(figsize=(5,5))
 
-boundary = utils.Rectangle(200,200,400,400)
+# specs of Rectangle:
+width = 400
+height = 400
+center_x = 0
+center_y = 0
+boundary = utils.Rectangle(center_x,center_y,width,height)
 qt = utils.Quadtree(boundary, 4)
 
-for i in range(100):
+for i in range(5500):
     x = np.random.randint(0,400)
     y = np.random.randint(0,400)
 
     p = utils.Point(x,y)
     ax.scatter(p.x,p.y,s=1,c='black')
     qt.insert(p)
-ax.set_xlim(-100,500)
-ax.set_ylim(-100,500)
+ax.set_xlim(center_x-width/2,center_x+width/2)
+ax.set_ylim(center_y-height/2,center_y+height/2)
 
 qt.show(ax)
 plt.show()
